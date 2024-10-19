f26b3d247bae30238bee8c444ed6e90b06f84ae0
Vera Vikhreva <vvikhreva.wp@gmail.com>
2024-10-19 19:26:19 +0200

    add e2e tests for /blogs .post .get .put .delete for /posts .post

--- __tests__/e2e/blogs.posts.api.test.ts
+++ __tests__/e2e/blogs.posts.api.test.ts
@@ -1,6 +1,7 @@
 import {SETTINGS} from "../../src/settings";
 import {app} from "../../src/app";
 import {agent} from "supertest";
+import {db} from "../../src/repositories/db";
 
 const req = agent(app);
 
@@ -12,47 +13,78 @@ function encodeAuth(){
 
 describe(SETTINGS.PATH.BLOGS, () => {
 
-    beforeAll(async()=> {
+    beforeAll(async () => {
         await req.delete("/testing/all-data")
     })
 
-    it("should get empty array", async() =>{
+    it("should get empty array", async () => {
         await req.get(SETTINGS.PATH.BLOGS)
             .expect(200, [])
     })
 
     // create request
 
-    let newBlog1:any = null;
-    it("should create a new element in the array", async() => {
+    let CreateData: any = null;
+    let CreateData2: any = null;
+    let newBlog1: any = null;
+    let newBlog2: any = null;
+    it("should create a new element in the array", async () => {
 
-        const newData = {
+        CreateData = {
             name: "Travel Blog",
             description: "Blog about traveling",
             websiteUrl: "https://www.travel-blog.com/"
         }
 
-        const res = await req
+        CreateData2 = {
+            name: "Photo Blog",
+            description: "Blog about photography",
+            websiteUrl: "https://www.photo-blog.com/"
+        }
+
+        const createRes = await req
             .post(SETTINGS.PATH.BLOGS)
             .set({'Authorization': 'Basic ' + encodeAuth()})
-            .send(newData)
+            .send(CreateData)
             .expect(201)
 
 
-        newBlog1 = res.body;
+        newBlog1 = createRes.body;
         expect(newBlog1).toEqual({
             id: expect.any(String),
-            name: newData.name,
-            description: newData.description,
-            websiteUrl: newData.websiteUrl
+            name: CreateData.name,
+            description: CreateData.description,
+            websiteUrl: CreateData.websiteUrl
 
         })
 
+        const createRes2 = await req
+            .post(SETTINGS.PATH.BLOGS)
+            .set({'Authorization': 'Basic ' + encodeAuth()})
+            .send(CreateData2)
+            .expect(201);
+
+        newBlog2 = createRes2.body;
+
+        expect(newBlog2).toEqual({
+            id: expect.any(String),
+            name: CreateData2.name,
+            description: CreateData2.description,
+            websiteUrl: CreateData2.websiteUrl
+
+
+        })
     })
 
-    it("should not create a new element in the array with incorrect data", async() => {
 
-        const newData = {
+
+
+
+
+
+    it("should not create a new element in the array with incorrect data", async () => {
+
+        const newCreateData = {
             name: 123,
             description: "Blog about traveling",
             websiteUrl: "https://www.travel-blog.com/"
@@ -61,15 +93,15 @@ describe(SETTINGS.PATH.BLOGS, () => {
         await req
             .post(SETTINGS.PATH.BLOGS)
             .set({'Authorization': 'Basic ' + encodeAuth()})
-            .send(newData)
+            .send(newCreateData)
             .expect(400)
 
 
-        })
+    })
 
     // get by id request
 
-    it("should get an existing element via id parameter", async() => {
+    it("should get an existing element via id parameter", async () => {
 
         await req
             .get(`${SETTINGS.PATH.BLOGS}/${newBlog1.id}`)
@@ -77,7 +109,7 @@ describe(SETTINGS.PATH.BLOGS, () => {
 
     })
 
-    it("should not get an element of array with wrong id parameter", async() => {
+    it("should not get an element of array with wrong id parameter", async () => {
 
         await req
             .get(SETTINGS.PATH.BLOGS + "/-1")
@@ -85,10 +117,115 @@ describe(SETTINGS.PATH.BLOGS, () => {
 
     })
 
+    // update
+
+
+    it("should update existing element in the array", async () => {
+
+        const UpdateData = {
+            name: "Cooking Blog",
+            description: "Blog about food",
+            websiteUrl: "https://www.cook-blog.com/"
+        }
+
+        await req
+            .put(`${SETTINGS.PATH.BLOGS}/${newBlog1.id}`)
+            .set({'Authorization': 'Basic ' + encodeAuth()})
+            .send(UpdateData)
+            .expect(204)
+
+        await req
+            .get(`${SETTINGS.PATH.BLOGS}/${newBlog1.id}`)
+            .expect(200, {
+                ...newBlog1,
+                name: UpdateData.name,
+                description: UpdateData.description,
+                websiteUrl: UpdateData.websiteUrl
+            })
+
+
+    })
+
+    let UpdateData2: any = null;
+
+    it("should not update existing element in the array with incorrect id provided", async () => {
+
+        UpdateData2 = {
+            name: "Beauty Blog",
+            description: "Blog about make-up",
+            websiteUrl: "https://www.beauty-blog.com/"
+        }
+
+        await req
+            .put(`${SETTINGS.PATH.BLOGS}/-1`)
+            .set({'Authorization': 'Basic ' + encodeAuth()})
+            .send(UpdateData2)
+            .expect(404)
+
+        await req
+            .get(`${SETTINGS.PATH.BLOGS}/${newBlog1.id}`)
+            .expect(200, {
+                ...newBlog1,
+                name: "Cooking Blog",
+                description: "Blog about food",
+                websiteUrl: "https://www.cook-blog.com/"
+            })
+
+
+        })
+
+    it("should delete element with given id", async () => {
+
+        await req
+            .delete(`${SETTINGS.PATH.BLOGS}/${newBlog1.id}`)
+            .set({'Authorization': 'Basic ' + encodeAuth()})
+            .expect(204)
+
+        console.log(db.blogs)
+
+        await req
+            .get(SETTINGS.PATH.BLOGS)
+            .expect(200, [newBlog2])
+    })
 
 
 
+})
 
+describe(SETTINGS.PATH.POSTS, () => {
+
+    it("should get empty array", async () => {
+        await req.get(SETTINGS.PATH.POSTS)
+            .expect(200, [])
+    })
+
+    let newPost: any = null;
+    it("should create an element with correct input data", async () => {
+        const postData = {
+            title: "New Post",
+            shortDescription: "Some description",
+            content: "Some content",
+            blogId: "2"
+        }
+
+        const res = await req
+            .post(SETTINGS.PATH.POSTS)
+            .set({'Authorization': 'Basic ' + encodeAuth()})
+            .send(postData)
+            .expect(201)
+
+        newPost = res.body;
+        expect(newPost).toEqual({
+            id: expect.any(String),
+            title: postData.title,
+            shortDescription: postData.shortDescription,
+            content: postData.content,
+            blogId: postData.blogId,
+            blogName: "Photo Blog"
+
+        })
+
+    })
 
 
 })
